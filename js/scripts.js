60857fe7be5c9f401eb8a89870d6256efe566fad
lampeduza <lmdogg@mail.ru>
2020-05-31 15:45:09 +0700

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var pageHeader = document.querySelector('.page-header');
+var pageHeaderToggle = document.querySelector('.page-header__toggle');
+
+pageHeader.classList.remove('page-header--no-js');
+
+pageHeaderToggle.addEventListener('click', function() {
+  pageHeader.classList.toggle('page-header--opened');
+});
